f5baa23856257cd89958966f6f453782e67826e1
bruslaan <bruslan.no@gmail.com>
2020-11-01 21:06:53 +0100

    some cache tests

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -81,6 +81,8 @@ function registerValidSW(swUrl: string, config?: Config) {
                 'New content is available and will be used when all ' +
                   'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
               );
+              window.location.reload()
+
 
               // Execute callback
               if (config && config.onUpdate) {
