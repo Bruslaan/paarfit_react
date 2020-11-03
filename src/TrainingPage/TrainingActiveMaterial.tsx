d3065e9156e4295d361cc2e14643f757d006836b
bruslaan <bruslan.no@gmail.com>
2020-11-03 21:24:42 +0100

    code refactored

--- src/TrainingPage/TrainingActiveMaterial.tsx
+++ src/TrainingPage/TrainingActiveMaterial.tsx
@@ -12,7 +12,7 @@ import { AuthContext } from '../AuthProvider'
 import { CircularProgress } from '@material-ui/core';
 import { Prompt } from 'react-router'
 import firebase, { heutigesDatum } from '../firebase'
-
+import { ReturnLink, HandleData } from './trainingsUtils'
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -46,23 +46,6 @@ const useStyles = makeStyles((theme: Theme) =>
 
 
 
-
-function getRandom(arr: any, n: number) {
-    var result = new Array(n),
-        len = arr.length,
-        taken = new Array(len);
-    if (n > len)
-        return arr
-    while (n--) {
-        var x = Math.floor(Math.random() * len);
-        result[n] = arr[x in taken ? taken[x] : x];
-        taken[x] = --len in taken ? taken[len] : len;
-    }
-    return result;
-}
-
-
-
 const LevelMapping: { [key: number]: string; } = {
     0: "Aufwärmen",
     1: "Kraft",
@@ -70,8 +53,6 @@ const LevelMapping: { [key: number]: string; } = {
     3: "Yoga",
     4: "Dehnen"
 }
-
-
 
 
 export default function VerticalLinearStepper() {
@@ -89,67 +70,7 @@ export default function VerticalLinearStepper() {
     let schwierigkeitsgrad: string = userInformation?.stufe
 
 
-    const ReturnLink = (stage: string, level: string) => {
 
-        let workoutsLevel = `&&workoutlevels.levelname=${level}`
-
-        switch (stage) {
-            case "Kraft":
-                return `https://paarfit-strapi.herokuapp.com/workouts?workoutcategories.categoryname=${stage}` + workoutsLevel
-            case "Ausdauer/Fettverbrennung":
-                return `https://paarfit-strapi.herokuapp.com/workouts?workoutbodyparts.bodypart=Ausdauer%20Pool%20Anf%C3%A4nger`
-            case "Yoga":
-                return `https://paarfit-strapi.herokuapp.com/workouts?workoutcategories.categoryname=${stage}`
-            case "Aufwärmen":
-                return `https://paarfit-strapi.herokuapp.com/workouts?workoutbodyparts.bodypart=Aufw%C3%A4rmen%20Anf%C3%A4nger%20Pool%20` + 1
-            case "Dehnen":
-                return `https://paarfit-strapi.herokuapp.com/workouts?workoutbodyparts.bodypart=Dehnpool%20Anf%C3%A4nger/Fortgeschrittene/Profis`
-            default:
-                break;
-        }
-    }
-
-    const CreateWorkouts = (object: Array<any>) => {
-
-        const BrustWorkouts = object.filter(workout => workout.workoutbodyparts[0].bodypart === "Brust")
-        const Beine = object.filter(workout => workout.workoutbodyparts[0].bodypart === "Beine")
-        const Rücken = object.filter(workout => workout.workoutbodyparts[0].bodypart === "Rücken")
-        const Schulter = object.filter(workout => workout.workoutbodyparts[0].bodypart === "Schulter")
-        const Arme = object.filter(workout => workout.workoutbodyparts[0].bodypart === "Arme")
-        const Bauch = object.filter(workout => workout.workoutbodyparts[0].bodypart === "Bauch")
-
-        const CompleteWorkout = [
-            ...getRandom(BrustWorkouts, 1),
-            ...getRandom(Beine, 1),
-            ...getRandom(Rücken, 1),
-            ...getRandom(Schulter, 1),
-            ...getRandom(Arme, 1),
-            ...getRandom(Bauch, 1)]
-
-        console.log("The workous", CompleteWorkout)
-
-        return CompleteWorkout
-    }
-
-    const HandleData = (stage: string, level: string, data: Array<any>) => {
-
-
-        switch (stage) {
-            case "Kraft":
-                return CreateWorkouts(data)
-            case "Ausdauer/Fettverbrennung":
-                return data
-            case "Yoga":
-                return getRandom(data, 1)
-            case "Aufwärmen":
-                return data
-            case "Dehnen":
-                return getRandom(data, 2)
-            default:
-                break;
-        }
-
-    }
 
 
     async function fetchWorkouts() {
---
+++ src/TrainingPage/trainingsUtils.tsx
@@ -0,0 +1,75 @@
+export const ReturnLink = (stage: string, level: string) => {
+
+    let workoutsLevel = `&&workoutlevels.levelname=${level}`
+
+    switch (stage) {
+        case "Kraft":
+            return `https://paarfit-strapi.herokuapp.com/workouts?workoutcategories.categoryname=${stage}` + workoutsLevel
+        case "Ausdauer/Fettverbrennung":
+            return `https://paarfit-strapi.herokuapp.com/workouts?workoutbodyparts.bodypart=Ausdauer%20Pool%20Anf%C3%A4nger`
+        case "Yoga":
+            return `https://paarfit-strapi.herokuapp.com/workouts?workoutcategories.categoryname=${stage}`
+        case "Aufwärmen":
+            return `https://paarfit-strapi.herokuapp.com/workouts?workoutbodyparts.bodypart=Aufw%C3%A4rmen%20Anf%C3%A4nger%20Pool%20` + 1
+        case "Dehnen":
+            return `https://paarfit-strapi.herokuapp.com/workouts?workoutbodyparts.bodypart=Dehnpool%20Anf%C3%A4nger/Fortgeschrittene/Profis`
+        default:
+            break;
+    }
+}
+export const HandleData = (stage: string, level: string, data: Array<any>) => {
+
+
+    switch (stage) {
+        case "Kraft":
+            return CreateWorkouts(data)
+        case "Ausdauer/Fettverbrennung":
+            return data
+        case "Yoga":
+            return getRandom(data, 1)
+        case "Aufwärmen":
+            return data
+        case "Dehnen":
+            return getRandom(data, 2)
+        default:
+            break;
+    }
+
+}
+
+export const CreateWorkouts = (object: Array<any>) => {
+
+    const BrustWorkouts = object.filter(workout => workout.workoutbodyparts[0].bodypart === "Brust")
+    const Beine = object.filter(workout => workout.workoutbodyparts[0].bodypart === "Beine")
+    const Rücken = object.filter(workout => workout.workoutbodyparts[0].bodypart === "Rücken")
+    const Schulter = object.filter(workout => workout.workoutbodyparts[0].bodypart === "Schulter")
+    const Arme = object.filter(workout => workout.workoutbodyparts[0].bodypart === "Arme")
+    const Bauch = object.filter(workout => workout.workoutbodyparts[0].bodypart === "Bauch")
+
+    const CompleteWorkout = [
+        ...getRandom(BrustWorkouts, 1),
+        ...getRandom(Beine, 1),
+        ...getRandom(Rücken, 1),
+        ...getRandom(Schulter, 1),
+        ...getRandom(Arme, 1),
+        ...getRandom(Bauch, 1)]
+
+    console.log("The workous", CompleteWorkout)
+
+    return CompleteWorkout
+}
+
+export function getRandom(arr: any, n: number) {
+    var result = new Array(n),
+        len = arr.length,
+        taken = new Array(len);
+    if (n > len)
+        return arr
+    while (n--) {
+        var x = Math.floor(Math.random() * len);
+        result[n] = arr[x in taken ? taken[x] : x];
+        taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
+}
+
