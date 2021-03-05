cc78b1f839f8a76392803518c1ab987da6728ed4
Julian <biscombe.j@gmail.com>
2021-03-05 08:10:44 +0100

    logout dropdown-menu, logo path dashboard

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -1,121 +1,122 @@
-import React, { useContext, useState } from "react";
-import { Route, Redirect, Switch } from "react-router-dom";
-import { AuthContext } from "./AuthProvider";
+import React, { useContext, useState } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import { AuthContext } from './AuthProvider';
 import Fab from '@material-ui/core/Fab';
-import NavBarContainer from './NavBarContainer'
-import { BottomNavBarContainer } from './BottomNavBar'
-import { SidebarNavTopContainer } from './sidebarNavTop'
-import { SidebarNavBottomContainer } from './sidebarNavBottom'
-import Dashboard from './DashboardPage'
-import ProfilePage from './ProfilePage'
-import CreateUserInformation from './CreateUserInformation'
-import TrainingPage from './TrainingPage'
-import TraningActive from './TrainingPage/TrainingActiveMaterial'
-import TrainingOverView from './TrainingPage/TrainingOverview'
+import NavBarContainer from './NavBarContainer';
+import { BottomNavBarContainer } from './BottomNavBar';
+import { SidebarNavTopContainer } from './sidebarNavTop';
+import { SidebarNavBottomContainer } from './sidebarNavBottom';
+import Dashboard from './DashboardPage';
+import ProfilePage from './ProfilePage';
+import CreateUserInformation from './CreateUserInformation';
+import TrainingPage from './TrainingPage';
+import TraningActive from './TrainingPage/TrainingActiveMaterial';
+import TrainingOverView from './TrainingPage/TrainingOverview';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import { Logo } from './Logo'
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+import { Logo } from './Logo';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import DescriptionIcon from '@material-ui/icons/Description';
-import Drawer from './react-bottom-drawer'
-import Tagebuch from './Tagebuch'
-
+import Drawer from './react-bottom-drawer';
+import Tagebuch from './Tagebuch';
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-
-        fab: {
-            position: 'fixed',
-            bottom: theme.spacing(10),
-            right: "10%",
-            zIndex: 10,
-        },
-
-    }),
+  createStyles({
+    fab: {
+      position: 'fixed',
+      bottom: theme.spacing(10),
+      right: '10%',
+      zIndex: 10,
+    },
+  })
 );
 
-
 const PrivateRoute = ({ ...rest }) => {
-    const customStyle = useStyles()
-    const { authenticated, loadingAuthState, haveInformation } = useContext(AuthContext);
-    const [drawerVisible, setdrawerVisible] = useState(false)
-    const openDrawer = () => {
-        console.log("hallo")
-        setdrawerVisible(true)
-    }
-    const closeDrawer = () => {
-        setdrawerVisible(false)
-    }
+  const customStyle = useStyles();
+  const { authenticated, loadingAuthState, haveInformation } = useContext(
+    AuthContext
+  );
+  const [drawerVisible, setdrawerVisible] = useState(false);
+  const openDrawer = () => {
+    console.log('hallo');
+    setdrawerVisible(true);
+  };
+  const closeDrawer = () => {
+    setdrawerVisible(false);
+  };
 
-    if (loadingAuthState) {
-        return (
-            <div className="center__all">
-                <Logo name="PaarFit" />
-                <CircularProgress />
-            </div>
-        );
-    }
-
+  if (loadingAuthState) {
     return (
-        <Route
-            {...rest}
-            render={routeProps =>
-                authenticated ? (
-                    haveInformation ? (
-                        <div>
-                            <NavBarContainer />
-
-                            <div className="spacer"></div>
-                            <div className="main__container mainContainerStyle">
-                                <div className="ctMenuLeft disable_on_mobile">
-                                    <SidebarNavTopContainer />
-                                    <SidebarNavBottomContainer/>
-                                </div>
-                                <div className="contentArea">
-                                    <Switch>
-                                        <Route exact path="/profile">
-                                            <ProfilePage />
-                                        </Route>
-                                        <Route exact path="/dashboard">
-                                            <Dashboard />
-                                        </Route>
-                                        <Route exact path="/training">
-                                            <TrainingPage />
-                                        </Route>
-                                        <Route exact path="/training/overview">
-                                            <TrainingOverView />
-                                        </Route>
-                                        <Route exact path="/training/overview/active/:id">
-                                            <TraningActive />
-                                        </Route>
-                                        <Route exact path="/">
-                                            <TrainingPage />
-                                        </Route>
-                                    </Switch>
-                                </div>
-
-                            </div>
-                            <BottomNavBarContainer />
-
-                            <Fab onClick={openDrawer} color="secondary" aria-label="add" className={customStyle.fab}>
-                                <DescriptionIcon />
-                            </Fab>
-
-                            <Drawer isVisible={drawerVisible} onClose={closeDrawer}>
-                                Trainingstagebuch
-                                    <div style={{ }}>
-                                    <Tagebuch />
-                                </div>
-                            </Drawer>
-                        </div>
-                    ) : (
-                            <CreateUserInformation />
-                        )
-                ) : (
-                        <Redirect to={{ pathname: "/auth", state: { prevPath: rest.path } }} />
-                    )
-            }
-        />
+      <div className='center__all'>
+        <Logo name='PaarFit' />
+        <CircularProgress />
+      </div>
     );
-}
+  }
 
-export default PrivateRoute
+  return (
+    <Route
+      {...rest}
+      render={(routeProps) =>
+        authenticated ? (
+          haveInformation ? (
+            <div>
+              <NavBarContainer />
+
+              <div className='spacer'></div>
+              <div className='main__container mainContainerStyle'>
+                <div className='ctMenuLeft disable_on_mobile'>
+                  <SidebarNavTopContainer />
+                  <SidebarNavBottomContainer />
+                </div>
+                <div className='contentArea'>
+                  <Switch>
+                    <Route exact path='/profile'>
+                      <ProfilePage />
+                    </Route>
+                    <Route exact path='/'>
+                      <Dashboard />
+                    </Route>
+                    <Route exact path='/training'>
+                      <TrainingPage />
+                    </Route>
+                    <Route exact path='/training/overview'>
+                      <TrainingOverView />
+                    </Route>
+                    <Route exact path='/training/overview/active/:id'>
+                      <TraningActive />
+                    </Route>
+                  </Switch>
+                </div>
+              </div>
+              <BottomNavBarContainer />
+
+              <Fab
+                onClick={openDrawer}
+                color='secondary'
+                aria-label='add'
+                className={customStyle.fab}
+              >
+                <DescriptionIcon />
+              </Fab>
+
+              <Drawer isVisible={drawerVisible} onClose={closeDrawer}>
+                Trainingstagebuch
+                <div style={{}}>
+                  <Tagebuch />
+                </div>
+              </Drawer>
+            </div>
+          ) : (
+            <CreateUserInformation />
+          )
+        ) : (
+          <Redirect
+            to={{ pathname: '/auth', state: { prevPath: rest.path } }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
--- src/sidebarNavBottom/index.tsx
+++ src/sidebarNavBottom/index.tsx
@@ -1,34 +1,29 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode } from 'react';
 
-import { Menu } from '../Menu'
-import { navLinksSidebarBottom } from '../constants'
-
-
+import { Menu } from '../Menu';
+import { navLinksSidebarBottom } from '../constants';
 
 interface Props {
-    items?: ReactNode
-
+  items?: ReactNode;
 }
 
 // sidebar Nav bar
 
 export const SidebarNavBottom: React.FC<Props> = (props) => {
-    const { items } = props
-    return (
-        <div className="detMenuLeftBottom whiteBg">
-           <div className="areaMenuLeftBottom blackBg">
-                {items}
-            </div>
-        </div>
-    )
-}
-const sidebarMenuBottomSt ='sidebarMenuTopSt';
+  const { items } = props;
+  return (
+    <div className='detMenuLeftBottom whiteBg'>
+      <div className='areaMenuLeftBottom blackBg'>{items}</div>
+    </div>
+  );
+};
+const sidebarMenuBottomSt = 'sidebarMenuTopSt';
 export const SidebarNavBottomContainer: React.FC<Props> = () => {
-    
-    return (
-        <SidebarNavBottom items={<Menu  links={navLinksSidebarBottom} menuType="sidebarMenuBottomSt" />} />
-    )
-}
-
-
-
+  return (
+    <SidebarNavBottom
+      items={
+        <Menu links={navLinksSidebarBottom} menuType='sidebarMenuBottomSt' />
+      }
+    />
+  );
+};
