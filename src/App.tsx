7959b03785e38e3377240c58d98a81237ffb47ce
Ruslan Noschajew <7447141-bruslan@users.noreply.gitlab.com>
2021-03-19 22:56:14 +0100

    Merged with julian landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,53 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import Store from './GlobalState/store';
 import Login from './Pages/LoginPage';
 import Register from './Pages/RegisterPage';
 import LandingPage from './Pages/LandingPage/LandingPage';
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    useRouteMatch, Redirect,
+} from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
-import {AuthContext} from "./AuthProvider";
-import Pricing from "./Pages/LandingPage/PricingPage/Pricing";
-
+import { AuthContext } from './AuthProvider';
+import Pricing from './Pages/LandingPage/PricingPage/Pricing';
 
 let App = () => {
-    const {authenticated, loadingAuthState, haveInformation} = useContext(
-        AuthContext
-    );
+  const { authenticated, loadingAuthState, haveInformation } = useContext(
+    AuthContext
+  );
 
-    if (loadingAuthState) {
-        return (
-            <div className="loadingOverlay">
-                <div className="loader">
-
-                </div>
-
-            </div>
-        )
-    }
-
-
+  if (loadingAuthState) {
     return (
-        <div>
-            <Router>
-                {
-                    !authenticated ?
-                        <Switch>
-                            <Route exact path='/auth' component={Login}/>
-                            <Route exact path='/register' component={Register}/>
-                            <Route exact path='/' component={LandingPage}/>
-                            <Route exact path='/pricing' component={Pricing}/>
-                            <Route path='/about' component={LandingPage} />
-                        </Switch>
-                        :
-                        <Switch>
-                            <Store>
-                                <PrivateRoute path='/'/>
-                            </Store>
-                        </Switch>
-                }
-
-
-            </Router>
-        </div>
+      <div className='loadingOverlay'>
+        <div className='loader'></div>
+      </div>
     );
+  }
+
+  return (
+    <div>
+      <Router>
+        {!authenticated ? (
+          <Switch>
+            <Route exact path='/auth' component={Login} />
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/' component={LandingPage} />
+            <Route path='/pricing' component={LandingPage} />
+            <Route path='/about' component={LandingPage} />
+              <Redirect to='/'></Redirect>
+          </Switch>
+        ) : (
+          <Switch>
+            <Store>
+              <PrivateRoute path='/' />
+            </Store>
+          </Switch>
+        )}
+      </Router>
+    </div>
+  );
 };
 
 export default App;
