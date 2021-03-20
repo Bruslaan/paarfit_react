f7d861317a82d441bb59fc386a474d8e837b8eba
Ruslan Noschajew <7447141-bruslan@users.noreply.gitlab.com>
2021-03-20 17:29:28 +0100

    Timer editted

--- src/Components/Timer/Timer.tsx
+++ src/Components/Timer/Timer.tsx
@@ -1,106 +1,85 @@
-import React, { useEffect, useState } from 'react';
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import Button from '@material-ui/core/Button';
+import React, {useEffect, useState} from 'react';
+import {CircularProgressbar} from 'react-circular-progressbar';
 
-const Timer = () => {
-  const [pause, setPause] = useState(false);
-  const [timer, setTimer] = useState(0);
-  const [timerValues, setTimerValues] = useState({
-    time: 5,
-    set: 2,
-    pause: 3,
-    progress: 0,
-  });
+const Timer = ({sets, pause = 3, trainingTime = 5}: any) => {
 
-  let setArr = [''];
-  const initialTime = timerValues.time;
 
-  const setTrainingSets = () => {
-    for (let step = 1; step <= timerValues.set; step++) {
-      // Runs 5 times, with values of step 0 through 4.
-      setArr.push('Set ' + step);
+    const SequenceArray = ["Workout", "Pause", "Workout", "Pause"]
+
+    const [timer, setTimer] = useState(0);
+    const [currentSequenceIndex, setCurrentSequenceIndex] = useState(0);
+
+    const decreaseTimer = () => {
+        setTimer(timer - 1)
     }
-  };
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      switch (pause) {
-        case true:
-          if (!timerValues.pause) {
-            setPause(false);
-            console.log('Pause over');
-          } else
-            setTimerValues({
-              ...timerValues,
-              time: timerValues.time - 1,
-              progress: (100 / initialTime) * timerValues.time,
-            });
-          break;
-        case false:
-          setTimerValues({
-            ...timerValues,
-            progress: 100,
-          });
-          if (!timerValues.time) {
-            setPause(true);
-            console.log('Workout over');
-          }
+    const mayDecreaseTimer = timer > 0
+    const mayIncreaseSequence = currentSequenceIndex < (SequenceArray.length - 1)
 
-          setTimerValues({
-            ...timerValues,
-            time: timerValues.time - 1,
-            progress: (100 / initialTime) * timerValues.time,
-          });
-          break;
-      }
-    }, 1000);
-    // Clear timeout if the component is unmounted
-    return () => clearTimeout(timer);
-  });
+    useEffect(() => {
+        let initTimerWith = 0
+        if (SequenceArray[currentSequenceIndex] === "Workout") {
+            initTimerWith = trainingTime
+        } else {
+            initTimerWith = pause
+        }
+        setTimer(initTimerWith)
+    }, [currentSequenceIndex]);
 
-  setTrainingSets();
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            console.log("have to incrase the seq")
+            if (!timer && mayIncreaseSequence) {
+                console.log("have to incrase the seq")
+                setCurrentSequenceIndex(currentSequenceIndex + 1)
+            } else {
+                mayDecreaseTimer && decreaseTimer()
+            }
+        }, 1000);
+        // Clear timeout if the component is unmounted
+        return () => clearTimeout(timer);
+    });
 
-  return (
-    <div>
-      <h2>{pause ? 'Workout' : 'Pause'}</h2>
-      <div className='boxTimeProgressbar'>
-        <div className='flipText'>
-          <CircularProgressbar
-            value={timerValues.progress}
-            text=''
-            styles={{
-              path: {
-                strokeWidth: '8',
-                stroke: '#9529F9',
-                strokeLinecap: 'round',
-                transition: 'stroke-dashoffset 0.5s ease 0s',
-              },
-              trail: {
-                strokeWidth: '4',
-                stroke: '#EAEBEB',
-                strokeLinecap: 'round',
-              },
-            }}
-          />
-        </div>
-        <div className='titleTimeProgressbar'>
-          <h3>
-            {pause ? `${timerValues.pause} Sek` : `${timerValues.time} Sek`}
-          </h3>
-          <p className='purple1'>{pause ? 'Go!' : 'Relax'}</p>
-        </div>
-      </div>
-      <div className='ctSetProgressbar'>
-        {setArr.map((set: any, key: any) => (
-          <>
-            <div key={key} className='boxSetProgressbar gray2'>
-              {set}
+
+    return (
+        <div>
+            <h2>{pause ? 'Workout' : 'Pause'}</h2>
+            <div className='boxTimeProgressbar'>
+                <div className='flipText'>
+                    <CircularProgressbar
+                        value={100}
+                        text=''
+                        styles={{
+                            path: {
+                                strokeWidth: '8',
+                                stroke: '#9529F9',
+                                strokeLinecap: 'round',
+                                transition: 'stroke-dashoffset 0.5s ease 0s',
+                            },
+                            trail: {
+                                strokeWidth: '4',
+                                stroke: '#EAEBEB',
+                                strokeLinecap: 'round',
+                            },
+                        }}
+                    />
+                </div>
+                <div className='titleTimeProgressbar'>
+                    <h3>
+                        {timer}
+                    </h3>
+                    <p className='purple1'>{pause ? 'Go!' : 'Relax'}</p>
+                </div>
             </div>
-          </>
-        ))}
-      </div>
-    </div>
-  );
+            <div className='ctSetProgressbar'>
+                {/*            {ArraySet.map((set: any, index: number) => (
+                    <div key={index} className='boxSetProgressbar gray2'>
+                        {set}
+                    </div>
+                ))}*/}
+            </div>
+        </div>
+    );
 };
 
 export default Timer;
