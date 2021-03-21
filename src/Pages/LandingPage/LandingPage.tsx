8ed0b6393c94cdf0b97c5b789d18dfaf3a2ef8ce
Julian <biscombe.j@gmail.com>
2021-03-21 17:25:39 +0100

    added navlink to navbar of landingpage

--- src/Pages/LandingPage/LandingPage.tsx
+++ src/Pages/LandingPage/LandingPage.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import About from './About/About';
 import Footer from './Footer/Footer';
 import Home from './Home';
@@ -11,11 +16,11 @@ const LandingPage = ({ path }: any) => {
 
   return (
     <div className='landingPage'>
-      <NavBar />
       <Router>
+        <NavBar />
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route path='/pricing' component={Pricing} />
+          <Route exact path='/pricing' component={Pricing} />
           <Route exact path='/about' component={About} />
           <Redirect to='/'></Redirect>
           {/* <Route exact path='/pricing' component={Magazin} /> */}
