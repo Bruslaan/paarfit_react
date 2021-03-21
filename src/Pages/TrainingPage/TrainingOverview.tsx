0870856d92568aa7f5180cd38327f82a7adcda64
Julian <biscombe.j@gmail.com>
2021-03-21 18:17:28 +0100

    trainingsoverview style

--- src/Pages/TrainingPage/TrainingOverview.tsx
+++ src/Pages/TrainingPage/TrainingOverview.tsx
@@ -1,50 +1,32 @@
-import React, {useContext, useEffect, useState} from 'react'
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import StepContent from '@material-ui/core/StepContent';
-import {useHistory} from "react-router-dom";
-import {Button} from '@material-ui/core';
-import {AuthContext} from '../../AuthProvider'
-import firebase, {heutigesDatum} from '../../firebase'
-import {sequenceState} from "./trainingsUtils"
-
-const style: any = {
-    width: "100%",
-    display: "flex",
-    flexWrap: 'wrap',
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-}
-
-const cardStyle = {
-    padding: "100px",
-    cursor: "pointer",
-}
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import './TrainingOverview.css';
 
 const TrainingOverview = () => {
-    const history = useHistory()
-    const startWorkout = (index: number) => {
-        history.push("/training/overview/active/" + index)
-    }
+  const history = useHistory();
+  const startWorkout = (index: number) => {
+    history.push('/training/overview/active/' + index);
+  };
 
-    const moods = ["Gut", "Sehr Gut", "Fantastisch"]
+  const moods = [
+    { title: 'Gut', description: 'Schwierigkeitsgrad I' },
+    { title: 'Sehr Gut', description: 'Schwierigkeitsgrad II' },
+    { title: 'Fantastisch', description: 'Schwierigkeitsgrad III' },
+  ];
 
-    return (
+  return (
+    <div className='trainingOverview'>
+      <h1>Wie geht es dir heute?</h1>
+      <div className='OverviewCardContainter'>
+        {moods.map((mood: any, index: number) => (
+          <div onClick={() => startWorkout(index)} className='cardStyle'>
+            <div className='trainingOverviewTitle'>{mood.title}</div>
+            <div className='trainingOverviewDes'>{mood.description}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-        <div style={{width: "100%", textAlign: "center"}}>
-            <h1>Wie geht es dir heute?</h1>
-            <div style={style}>
-                {
-                    moods.map((mood: string, index: number) => <div onClick={() => startWorkout(index)}
-                                                                    style={cardStyle}><h1>{mood}</h1></div>)
-                }
-            </div>
-        </div>
-
-    )
-}
-
-
-export default TrainingOverview
+export default TrainingOverview;
