2860609ae81928f9a44ea530f433e5c224414ae8
Ruslan Noschajew <7447141-bruslan@users.noreply.gitlab.com>
2021-04-04 20:36:53 +0200

    firebase roots changed

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 import 'firebase/storage';
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAPEP1AFFg111oeqeVSUcvjRvAorO06-Uc',
-  authDomain: 'paarfit-c3029.firebaseapp.com',
-  databaseURL: 'https://paarfit-c3029.firebaseio.com',
-  projectId: 'paarfit-c3029',
-  storageBucket: 'paarfit-c3029.appspot.com',
-  messagingSenderId: '462160346735',
-  appId: '1:462160346735:web:5100a0be1766cd1625579a',
-  measurementId: 'G-GBNWN1WNZ1',
+    apiKey: "AIzaSyAZQYO70xs-gNoDipQoiZHylwlINbzk4JM",
+    authDomain: "ruslantests.firebaseapp.com",
+    databaseURL: "https://ruslantests.firebaseio.com",
+    projectId: "ruslantests",
+    storageBucket: "ruslantests.appspot.com",
+    messagingSenderId: "853046165041",
+    appId: "1:853046165041:web:0cc5a9878fcc1a0bef9f10",
+    measurementId: "G-LCTS5S87TP"
 };
 // Initialize Firebase
 export const app = firebase.initializeApp(firebaseConfig);
