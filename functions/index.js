cb18863d019ec3c3f17fa1ddb13212e261568c97
Ruslan Noschajew <7447141-bruslan@users.noreply.gitlab.com>
2021-04-05 10:05:25 +0200

    Payment added

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,53 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+const cors = require('cors')({origin: true});
+app.use(cors);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const braintree = require("braintree");
+
+const gateway = new braintree.BraintreeGateway({
+    environment: braintree.Environment.Sandbox,
+    merchantId: "c8nsbmv7p2q24tv9",
+    publicKey: "f6q9dwp7fvnvy4rx",
+    privateKey: "3d2ec2affe5afc5dc4f9562b86aba5aa"
 });
+
+app.get("/client_token", (req, res) => {
+    gateway.clientToken.generate({}).then(response => {
+        res.send({token: response.clientToken});
+    });
+});
+
+app.post("/checkout", async (req, res) => {
+    const nonceFromTheClient = req.body.payment_method_nonce;
+
+    let result
+    try {
+        result = await gateway.customer.create({
+            firstName: "Charity3",
+            lastName: "Smith3",
+            paymentMethodNonce: nonceFromTheClient,
+        })
+    } catch (error) {
+        console.log("Cannot create user ", error)
+        res.send({status: "Failed"})
+    }
+
+    console.log(result)
+    // Use payment method nonce here
+    gateway.subscription.create({
+        paymentMethodToken: result["customer"].paymentMethods[0].token,
+        planId: "bulatschki",
+
+    }).then(result => {
+        res.send({status: result})
+    }).catch((error) => res.send({error: error}))
+});
+
+app.get("/getAllPlans", async (req, res) => {
+
+    const plays = await gateway.plan.all()
+    res.send({test: plays});
+});
+exports.payment = functions.https.onRequest(app);
---
+++ src/Components/Payment/payment.tsx
@@ -0,0 +1,65 @@
+import React, {useContext, useEffect, useState} from 'react'
+
+const dropin = require('braintree-web-drop-in');
+
+function PaymentComponent() {
+
+
+    useEffect(() => {
+
+
+        async function initPayment() {
+
+            const response = await fetch("http://localhost:5001/ruslantests/us-central1/payment/client_token")
+            const responseJson = await response.json()
+            const token = responseJson?.token
+            if (!token) {
+                return
+            }
+
+            let button = document.querySelector('#submit-button');
+
+            dropin.create({
+                authorization: token,
+                selector: '#dropin-container',
+                paypal: {
+                    currency: "EUR",
+                    amount: "22",
+                    flow: "checkout"
+                }
+
+            }, function (err: any, instance: any) {
+
+                button?.addEventListener('click', function () {
+                    instance.requestPaymentMethod(async function (err: any, payload: any) {
+                        // Submit payload.nonce to your server
+                        console.log("Called")
+                        const response = await fetch("http://localhost:5001/ruslantests/us-central1/payment/checkout", {
+                            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                            headers: {
+                                'Content-Type': 'application/json'
+                                // 'Content-Type': 'application/x-www-form-urlencoded',
+                            },
+                            body: JSON.stringify({"payment_method_nonce": payload?.nonce}) // body data type must match "Content-Type" header
+                        });
+
+                        console.log("response ", await response.json())
+                    });
+                })
+            });
+        }
+
+        initPayment()
+
+    }, []);
+
+
+    return (
+        <div>
+            <div id="dropin-container"></div>
+            <button id="submit-button" className="button button--small button--green">Purchase</button>
+        </div>
+    )
+}
+
+export default PaymentComponent
