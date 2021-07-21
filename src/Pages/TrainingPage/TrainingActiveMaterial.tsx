e98c118b4d57d3487df9a78ac443bab5cec4cdc9
Ruslan Noschajew <7447141-bruslan@users.noreply.gitlab.com>
2021-07-21 18:01:39 +0200

    Multiplicator added

--- src/Pages/TrainingPage/TrainingActiveMaterial.tsx
+++ src/Pages/TrainingPage/TrainingActiveMaterial.tsx
@@ -181,7 +181,7 @@ export default function VerticalLinearStepper({stageNumber, onFinished}: any) {
 
         const pause = moodBased(currentMood, pausePause)
         newWorkout.set = proPerson ? moodBased(currentMood, parseSets) * 2 : moodBased(currentMood, parseSets)
-        newWorkout.rep = (kraft ? moodBased(currentMood, parseReps) * 3 : moodBased(currentMood, parseReps))*multiplicator
+        newWorkout.rep = Math.ceil((kraft ? moodBased(currentMood, parseReps) * 3 : moodBased(currentMood, parseReps)) * multiplicator)
         newWorkout.pause = proPerson ? 20 : pause
         newWorkout.alternate = proPerson
         return newWorkout
